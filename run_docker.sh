a58194212f954f51a9b59745c7a7175c4d1e1645
chris <kchrisj@gmail.com>
2021-02-16 01:02:04 +0000

    update run_docker file,config file

--- run_docker.sh
+++ run_docker.sh
@@ -3,9 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=joepub/capstone .
+docker build --tag=hello .
+
 
 # Step 2:
 # Push image to a docker repository
 docker container commit $(docker ps -qa) joepub/capstone
-docker push joepub/capstone
+docker push joepub/capstone:hello
