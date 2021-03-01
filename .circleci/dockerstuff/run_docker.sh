a0e6d911b95da95fcb8e3e1bf8ac5b98a2912c8b
chris <kchrisj@gmail.com>
2021-03-01 19:34:07 +0000

    Dockerfile and run_docker.sh

--- .circleci/dockerstuff/run_docker.sh
+++ .circleci/dockerstuff/run_docker.sh
@@ -13,7 +13,10 @@ docker build --tag=capstone .
 # Step 3:
 # List docker images
 docker image ls
+docker images --filter reference=capstone
 
 # Step 4:
 # Run flask_app
 #docker run --name capstone -p 8000:8000  -p 50000:50000 hello
+#docker run -t -i -p 8000:80 hello
+#CMD ["python", "flask_app/webapp.py"]
